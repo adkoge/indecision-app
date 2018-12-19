f1d98b69003ec2cd1b2af264ba83495cdd23c534
Alan Koger <adkoge@gmail.com>
2018-12-18 22:14:44 -0500

    Added onClick functions to buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,43 +1,83 @@
 "use strict";
 
-// arguments object - no longer bound with arrow functions
+console.log("My app is running");
 
-// const add2 = function(a, b) {
-//   console.log(arguments);
-//   return a + b;
-// };
-
-// console.log(add2(22, 122, 2222));
-
-// const add = (a, b) => {
-//   // console.log(arguments);
-//   return a + b;
-// };
-// console.log(add(55, 1, 1001));
-
-// this keyword - no longer bound
-
-// const user = {
-//   name: "Andrew",
-//   cities: ["Philadelphia", "New York", "Dublin"],
-//   printPlacesLived() {
-//     return this.cities.map(city => this.name + " has live in " + city);
-//   }
-// };
-// console.log(user.printPlacesLived());
-
-// Challenge area
-
-var multiplier = {
-  numbers: [2, 5, 8, 20],
-  multiplyBy: 2,
-  multiply: function multiply() {
-    var _this = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this.multiplyBy;
-    });
-  }
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer",
+  options: ["One", "Two"]
 };
 
-console.log(multiplier.multiply());
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "ul",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  ),
+  app.options.length > 0 ? "Here are your options" : "No options"
+);
+
+var count = 0;
+var addOne = function addOne() {
+  console.log('addOne');
+};
+var minusOne = function minusOne() {
+  console.log('minusOne');
+};
+var reset = function reset() {
+  console.log('reset');
+};
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Count: ",
+    count
+  ),
+  React.createElement(
+    "button",
+    { onClick: addOne },
+    "+1"
+  ),
+  React.createElement(
+    "button",
+    { onClick: minusOne },
+    "-1"
+  ),
+  React.createElement(
+    "button",
+    { onClick: reset },
+    "reset"
+  )
+);
+
+// Challenge
+// Make button "-1"
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -18,28 +18,29 @@ const template = (
   </div>
 );
 
-const user = {
-  name: "Alan",
-  age: 20,
-  location: "Richmond"
-};
-
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  } else {
-    return undefined;
-  }
+let count = 0;
+const addOne = () => {
+  console.log('addOne');
+}
+const minusOne = () => {
+  console.log('minusOne');
+}
+const reset = () => {
+  console.log('reset');
 }
 
 const templateTwo = (
   <div>
-    <h1>{user.name ? user.name : "Anonymous"}</h1>
-    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>reset</button>
   </div>
 );
 
+// Challenge
+// Make button "-1"
+
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
