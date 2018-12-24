de6afeb81e94b658456c92b8f25ca28856d16014
Alan Koger <adkoge@gmail.com>
2018-12-24 17:10:26 -0500

    Worked with classes, subclasses, and overriding parent class methods

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -14,8 +14,42 @@ class Person {
   }
 }
 
-const me = new Person('Alan Koger', 31);
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;  
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
 
-const other = new Person();
-console.log(other.getDescription());
+    if (this.hasMajor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age, homeLocation);
+    this.homeLocation = homeLocation;
+  }
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if(this.homeLocation) {
+      greeting += ` I'm visiting from ${this.homeLocation}.`;
+    }
+    return greeting;
+  }
+}
+
+const me = new Traveler('Alan Koger', 31, "Martinsville");
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, "Nowhere");
+console.log(other.getGreeting());
