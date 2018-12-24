174f1a66f36c3b73ff77921caf97178f7b8de647
Alan Koger <adkoge@gmail.com>
2018-12-24 17:22:02 -0500

    Wrote some simple React Components

---
+++ src/playground/jsx-indecision.js
@@ -0,0 +1,62 @@
+console.log("My app is running");
+
+const app = {
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer",
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    renderOptionsApp();
+  }
+};
+
+const removeAllItems = () => {
+  app.options = [];
+  renderOptionsApp();
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
+const appRoot = document.getElementById("app");
+
+const renderOptionsApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={removeAllItems}>Remove All</button>
+      {<p>1</p>}
+      <ol>
+        {
+          app.options.map(option => {
+            return (
+              <li key={option}>{option}</li>
+            )
+          })
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+}
+
+renderOptionsApp();
