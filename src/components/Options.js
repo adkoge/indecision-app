7775e5317d06c91b675c9ad39c302506983d2154
Alan Koger <adkoge@gmail.com>
2018-12-26 17:09:10 -0500

    Added styling to buttons

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,7 +3,12 @@ import Option from "./Option";
 
 const Options = (props) => (
     <div>
-      <button onClick={props.handleDeleteOptions}>Remove all</button>
+      <button 
+        onClick={props.handleDeleteOptions}
+        className="button button--link"  
+      >
+        Remove all
+      </button>
       {props.options.length === 0 && <p>Please add an option to get started!</p>}
       {props.options.map(option => (
         <Option
