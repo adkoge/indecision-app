994bf57721b9daafc6d5f3e68c20aed861f11b86
Alan Koger <adkoge@gmail.com>
2018-12-26 13:54:15 -0500

    Move classes/components into individual component files

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
-// import subtract, { square, add } from "./utils.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisionApp from "./components/IndecisionApp";
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(4,1));
-// console.log(subtract(100, 81));
-
-import isSenior, { isAdult, canDrink } from "./person";
-isAdult(19);
-canDrink(12);
-isSenior(64);
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
